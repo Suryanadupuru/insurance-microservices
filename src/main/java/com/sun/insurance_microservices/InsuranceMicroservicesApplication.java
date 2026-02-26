25bd1f12f9bae1518ce3d206fc37a61f5e86366d
Surya <suryajohnnadupuru@gmail.com>
2026-02-26 11:33:17 +0530

    Created centralized project

---
+++ src/main/java/com/sun/insurance_microservices/InsuranceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.sun.insurance_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sun/insurance_microservices/InsuranceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.insurance_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
