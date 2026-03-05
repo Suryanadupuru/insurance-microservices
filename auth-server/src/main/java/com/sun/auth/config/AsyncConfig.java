4c5dbb85ed10e2e4177185ea4b3d579a2afbf00d
Surya <suryajohnnadupuru@gmail.com>
2026-03-05 22:26:32 +0530

    AsyncConfig edited

--- auth-server/src/main/java/com/sun/auth/config/AsyncConfig.java
+++ auth-server/src/main/java/com/sun/auth/config/AsyncConfig.java
@@ -14,6 +14,7 @@ import java.util.concurrent.Executor;
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+	
 
     @Bean(name = "emailTaskExecutor")
     public Executor emailTaskExecutor() {
